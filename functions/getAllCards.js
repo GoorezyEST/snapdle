b5e50e7af0948ecf38240023db6d945bb666b96f
GoorezyEST <goorezyestudios@gmail.com>
2023-12-27 19:01:07 -0300

    Function to retrieve all the cards

---
+++ functions/getAllCards.js
@@ -0,0 +1,9 @@
+export async function retrieveCards() {
+  const response = await fetch("http://localhost:3000/api/cards")
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(`Error: ${error}`);
+    });
+
+  return response;
+}
