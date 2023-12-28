edba5704caa2388b7255c1490bf18709bc69c0e8
GoorezyEST <goorezyestudios@gmail.com>
2023-12-28 19:39:40 -0300

    Classic game mode page, showing only the guess by hints component

---
+++ app/classic-mode/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import GuessByHints from "../components/layouts/GuessByHints";
+
+function ClassicGameMode() {
+  return (
+    <main>
+      <GuessByHints />
+    </main>
+  );
+}
+
+export default ClassicGameMode;
