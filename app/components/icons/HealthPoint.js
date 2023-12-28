9fe70a1d98108b82a3a684c2f8475c023643f946
GoorezyEST <goorezyestudios@gmail.com>
2023-12-28 19:40:09 -0300

    Added another useful SVGs for the game

--- app/components/icons/HealthPoint.js
+++ app/components/icons/HealthPoint.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function HealthPoint() {
+export function HealthPoint({ color }) {
   return (
     <svg
       width="24"
@@ -11,57 +11,8 @@ export function HealthPoint() {
     >
       <path
         d="M12.0315 6.25876C12.0315 6.25876 10.5315 3.06226 7.15649 3.06226C3.46874 3.06226 1.53149 6.18751 1.53149 9.31201C1.53149 14.4998 12.0315 20.937 12.0315 20.937C12.0315 20.937 22.4685 14.562 22.4685 9.31201C22.4685 6.12451 20.469 3.06226 16.9065 3.06226C13.344 3.06226 12.0315 6.25876 12.0315 6.25876Z"
-        fill="url(#paint0_radial_687_6)"
+        fill={color}
       />
-      <defs>
-        <radialGradient
-          id="paint0_radial_687_6"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(12 12) rotate(138.013) scale(13.4536 15.7585)"
-        >
-          <stop stopColor="#FF0D0D" />
-          <stop offset="0.800337" stopColor="#FF4343" />
-          <stop offset="1" stopColor="#FF0D0D" />
-        </radialGradient>
-      </defs>
-    </svg>
-  );
-}
-
-export function HealthPointLoss() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M12.0315 6.25876C12.0315 6.25876 10.5315 3.06226 7.15649 3.06226C3.46874 3.06226 1.53149 6.18751 1.53149 9.31201C1.53149 14.4998 12.0315 20.937 12.0315 20.937C12.0315 20.937 22.4685 14.562 22.4685 9.31201C22.4685 6.12451 20.469 3.06226 16.9065 3.06226C13.344 3.06226 12.0315 6.25876 12.0315 6.25876Z"
-        fill="#1A1A1A"
-      />
-      <path
-        d="M12.0315 6.25876C12.0315 6.25876 10.5315 3.06226 7.15649 3.06226C3.46874 3.06226 1.53149 6.18751 1.53149 9.31201C1.53149 14.4998 12.0315 20.937 12.0315 20.937C12.0315 20.937 22.4685 14.562 22.4685 9.31201C22.4685 6.12451 20.469 3.06226 16.9065 3.06226C13.344 3.06226 12.0315 6.25876 12.0315 6.25876Z"
-        fill="url(#paint0_radial_687_4)"
-      />
-      <defs>
-        <radialGradient
-          id="paint0_radial_687_4"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(12 12) rotate(138.013) scale(13.4536 15.7585)"
-        >
-          <stop stopColor="#1A1A1A" />
-          <stop offset="0.800337" stopColor="#3B3B3B" />
-          <stop offset="1" stopColor="#1A1A1A" />
-        </radialGradient>
-      </defs>
     </svg>
   );
 }
