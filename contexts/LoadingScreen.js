a2b59c06cdc625eed49e822c4b446d0373e8a0e6
GoorezyEST <goorezyestudios@gmail.com>
2023-12-28 19:40:24 -0300

    Styled the loading screen

--- contexts/LoadingScreen.js
+++ contexts/LoadingScreen.js
@@ -2,9 +2,24 @@
 
 import React from "react";
 import { useGlobal } from "./GlobalContext";
+import MainLogo from "@/app/components/brand-assets/MainLogo";
 
 function LoadingScreen({ children }) {
-  return <>{children}</>;
+  const { isHydrated } = useGlobal();
+
+  return (
+    <>
+      {!isHydrated && (
+        <div className="loading">
+          <div className="loading_logo">
+            <MainLogo />
+          </div>
+          <span className="loading_loader"></span>
+        </div>
+      )}
+      {children}
+    </>
+  );
 }
 
 export default LoadingScreen;
