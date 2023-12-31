9bc1b8d57b5ce42897b148e1b9de1d265c768625
GoorezyEST <goorezyestudios@gmail.com>
2023-12-31 18:43:00 -0300

    Main game mode component, similar to "classic mode" but with another modality

---
+++ app/components/layouts/GuessBySkills.js
@@ -0,0 +1,186 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { useGlobal } from "@/contexts/GlobalContext";
+import styles from "@/styles/modules/guess-by-hint.module.css";
+import GameDashboard from "../utils/GameDashboard";
+import GameAutoSuggest from "../utils/GameAutoSuggest";
+import GameImageHint from "../utils/GameImageHint";
+import GameLoseModal from "../utils/GameLoseModal";
+import MusicOff from "../icons/MusicOff";
+import MusicOn from "../icons/MusicOn";
+import SoundOff from "../icons/SoundOff";
+import SoundOn from "../icons/SoundOn";
+import Return from "../icons/Return";
+import GameSkillHint from "../utils/GameSkillHint";
+
+function GuessBySkills() {
+  const {
+    cardsList,
+    randomCard,
+    setRandomCard,
+    userLoses,
+    toggleMutedMusic,
+    retrieveMutedMusic,
+    toggleMutedEffects,
+    retrieveMutedEffects,
+  } = useGlobal();
+
+  useEffect(() => {
+    if (cardsList !== null && randomCard === null) {
+      setRandomCard(cardsList[Math.floor(Math.random() * cardsList.length)]);
+    }
+  }, [cardsList]);
+
+  const { setIsHydrated } = useGlobal();
+
+  // Use useMemo to memoize the menuMusic instance
+  const menuMusic = useMemo(
+    () =>
+      new Howl({
+        src: ["/audio/game-music.mp3"],
+        volume: 0.03,
+        loop: true,
+      }),
+    []
+  );
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (!retrieveMutedMusic()) {
+      menuMusic.play();
+      setIsPlaying(true);
+    }
+
+    return () => {
+      menuMusic.stop();
+    };
+  }, []);
+
+  const handleMusicToggle = () => {
+    if (isPlaying) {
+      menuMusic.stop();
+      toggleMutedMusic(true);
+      setIsMusicMuted(true);
+    } else {
+      menuMusic.play();
+      toggleMutedMusic(false);
+      setIsMusicMuted(false);
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    if (randomCard !== null) {
+      setIsHydrated(true);
+    }
+  }, [randomCard]);
+
+  const clickSound = useMemo(
+    () =>
+      new Howl({
+        src: ["/audio/menu-click.ogg"],
+        volume: 0.03,
+      }),
+    []
+  );
+
+  const handleClickSound = (callback) => {
+    if (!retrieveMutedEffects()) {
+      clickSound.play();
+    }
+
+    if (callback) {
+      callback();
+    }
+  };
+
+  const hoverSound = useMemo(
+    () =>
+      new Howl({
+        src: ["/audio/menu-hover.ogg"],
+        volume: 0.02,
+      }),
+    []
+  );
+
+  const handleBtnHover = () => {
+    if (!retrieveMutedEffects()) {
+      hoverSound.play();
+    }
+  };
+
+  const handleEffectsToggle = () => {
+    if (retrieveMutedEffects()) {
+      toggleMutedEffects(false);
+      setAreEffectsMuted(false);
+    } else {
+      toggleMutedEffects(true);
+      setAreEffectsMuted(true);
+    }
+  };
+
+  const [areEffectsMuted, setAreEffectsMuted] = useState(
+    retrieveMutedEffects()
+  );
+
+  const [isMusicMuted, setIsMusicMuted] = useState(retrieveMutedMusic());
+
+  return (
+    <section className={styles.wrapper}>
+      {randomCard === null ? (
+        <h1 style={{ color: "white" }}>CARGANDO</h1>
+      ) : (
+        <div className={styles.container}>
+          {userLoses && <GameLoseModal />}
+          <div className={styles.game_container_wrapper}>
+            <img
+              className={styles.game_container_image}
+              src="https://i.imgur.com/P63TZaA.jpg"
+              alt="Background image"
+            />
+            <ul className={styles.game_settings}>
+              <li>
+                <span
+                  onClick={() => handleClickSound(handleMusicToggle)}
+                  onMouseEnter={handleBtnHover}
+                >
+                  <button className="primary_cta">
+                    {isMusicMuted ? <MusicOff /> : <MusicOn />}
+                  </button>
+                </span>
+              </li>
+              <li>
+                <span
+                  onClick={() => handleClickSound(handleEffectsToggle)}
+                  onMouseEnter={handleBtnHover}
+                >
+                  <button className="primary_cta">
+                    {areEffectsMuted ? <SoundOff /> : <SoundOn />}
+                  </button>
+                </span>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  onClick={() => handleClickSound()}
+                  onMouseEnter={handleBtnHover}
+                >
+                  <button className="primary_cta">
+                    <Return />
+                  </button>
+                </a>
+              </li>
+            </ul>
+            <div className={styles.game_container}>
+              <GameSkillHint />
+              <GameDashboard />
+              <GameAutoSuggest />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default GuessBySkills;
