a3b349ee5d9092fc5ad29828100d9e818956cd2e
GoorezyEST <goorezyestudios@gmail.com>
2024-01-07 16:42:28 -0300

    Deleted unused code

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 "use client";
 
-import styles from "@/styles/modules/home.module.css";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useGlobal } from "@/contexts/GlobalContext";
 import GameMenu from "./components/layouts/GameMenu";
 
 export default function Home() {
-  const { setIsHydrated, songsAndEffects, menuMusic } = useGlobal();
+  const { setIsHydrated } = useGlobal();
 
   useEffect(() => {
     setIsHydrated(true);
