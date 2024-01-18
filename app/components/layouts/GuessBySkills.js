aa72a700c693b81ce7fefd2054f9073e46a243af
GoorezyEST <goorezyestudios@gmail.com>
2024-01-18 15:52:04 -0300

    Added the resolution error for all the game modes

--- app/components/layouts/GuessBySkills.js
+++ app/components/layouts/GuessBySkills.js
@@ -143,10 +143,55 @@ function GuessBySkills() {
     };
   }, []);
 
+  const [isViewportBelowThreshold, setIsViewportBelowThreshold] =
+    useState(false);
+
+  useEffect(() => {
+    // Function to check if viewport height is below 650px
+    const checkViewportHeight = () => {
+      const viewportHeight = window.innerHeight;
+      const threshold = 630;
+      const belowThreshold = viewportHeight < threshold;
+
+      setIsViewportBelowThreshold(belowThreshold);
+    };
+
+    // Initial check when component mounts
+    checkViewportHeight();
+
+    // Event listener for window resize
+    const handleResize = () => {
+      checkViewportHeight();
+    };
+
+    // Attach resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className={styles.wrapper}>
       {randomCard === null ? (
-        <h1 style={{ color: "white" }}>CARGANDO</h1>
+        <div className="game_loading_wrapper">
+          <span>...</span>
+        </div>
+      ) : isViewportBelowThreshold ? (
+        <div className={styles.invalid_device_wrapper}>
+          <div className={styles.invalid_device}>
+            <h1>Ups...</h1>
+            <p>
+              We regret to inform you that this game mode does not work in this
+              resolution.
+            </p>
+            <Link href="/">
+              <button>Go back</button>
+            </Link>
+          </div>
+        </div>
       ) : (
         <div className={styles.container}>
           {userLoses && <GameLoseModal />}
